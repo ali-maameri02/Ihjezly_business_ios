6f52632b1e3939ad519af3c35898aaf5d1793aa0
mhxify <mohamedbenouarzeg1@gmail.com>
2026-02-06 04:39:12 +0100

    Initial Commit

---
+++ IHJZLYBusinessApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IHJZLYBusinessApp
+//
+//  Created by Mohamed Ali Benouarzeg on 6/2/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IHJZLYBusinessApp/IHJZLYBusinessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IHJZLYBusinessAppApp.swift
+//  IHJZLYBusinessApp
+//
+//  Created by Mohamed Ali Benouarzeg on 6/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct IHJZLYBusinessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
